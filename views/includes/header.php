276172fdba5f630da1a17c7f363327a06de0f143
omdousmane <o_diallo13@hetic.eu>
2022-06-24 17:16:16 +0200

    mise a jour des media queries du header

--- views/includes/header.php
+++ views/includes/header.php
@@ -13,4 +13,25 @@
          <img src="/Public/assets/icons/icon-profil.svg" alt="Utilisateur">
        </a>
      </nav>
+
+     <section class="header-menu__mobile slide-bottom">
+       <ul class="nav--menu">
+         <li class="item">
+           <a href="">Accueil </a>
+           <img src="./assets/house-chimney-solid.svg" alt="home">
+         </li>
+         <li class="item">
+           <a href="">Parrcourir</a>
+           <img src="./assets/magnifying-glass-solid.svg" alt="search">
+         </li>
+         <li class="item">
+           <a href="">Compte</a>
+           <img src="./icons/user-solid.svg" alt="user">
+         </li>
+         <li class="item">
+           <a href="">Panier</a>
+           <img src="./assets/shopping-basket-solid.svg" alt="panier">
+         </li>
+       </ul>
+     </section>
    </header>
