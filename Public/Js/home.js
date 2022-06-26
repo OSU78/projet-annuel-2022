50af339df4c30848e2824dfca4c8ea771047f330
CamilleMrntz <94170738+CamilleMrntz@users.noreply.github.com>
2022-06-26 19:37:19 +0200

    changement sur la home, bg-top added, js photo swipe added, footer responsive

---
+++ Public/Js/home.js
@@ -0,0 +1,28 @@
+
+
+// let leftArrow = document.getElementsByClassName('left');
+// let rightArrow = document.getElementsByClassName('prev');
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("artwork__scroll--img");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block"; 
+}
